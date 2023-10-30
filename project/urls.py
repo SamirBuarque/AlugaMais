06f3d80a6898c7d6ab398aed67a8680a015a8043
SamirBuarque <buarquesamir@gmail.com>
2023-10-30 15:30:18 -0300

    start app

--- project/urls.py
+++ project/urls.py
@@ -16,7 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse, HttpResponseForbidden
+
+def test(request):
+    return HttpResponseForbidden('Acesso negado')
+
+def home(request):
+    return HttpResponse("Pagina home em criação :D")
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
+    path('test/', test)
 ]
